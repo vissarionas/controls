85420acbf548cd57cf3fcad300e73bbc2dbdca5f
Vissarion Tsitoglou <vissarionas@gmail.com>
2018-11-29 18:36:11 +0100

    Add constants file

--- src/app.jsx
+++ src/app.jsx
@@ -1,40 +1,49 @@
 import React from 'react';
 import Excel from 'exceljs';
-
+import { styles, texts } from './constants';
 export default class App extends React.Component {
 
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
     this.state = {
-      files: ''
+      reports: '',
+      output: '',
     };
   }
 
   handleChange(event) {
-    this.setState({ files: event.target.files })
+    this.setState({
+      reports: event.target.files,
+      output: event.target.files
+    })
   }
 
-  readExcel(files) {
+  runControls(reports, output) {
     var workbook = new Excel.Workbook();
-    workbook.xlsx.readFile(files[0].path)
+    workbook.xlsx.readFile(reports[0].path)
       .then(function(workbook) {
         console.log(workbook.getWorksheet(1).getRow(1).values);
     });
   }
 
   render() {
+    console.log(styles);
     return (
-      <div>
+      <div style={styles.container}>
         <form onSubmit={ event => {
           event.preventDefault()
-          this.readExcel(this.state.files)
+          this.runControls(this.state.reports, this.state.output)
         }}>
-          <label>
-            Browse excel files:
-            <input type="file" accept=".xls, .xlsx" onChange={ event => this.handleChange(event) } multiple/>
+          <label style={styles.title}>
+            {texts.reportsLabel}
+            <input style={styles.input} type="file" accept=".xls, .xlsx" onChange={ event => this.handleChange(event) } multiple/>
           </label>
-          <input type="submit" value="Do something"></input>
+          <label style={styles.title}>
+            {texts.outputLabel}
+            <input style={styles.input} type="file" accept=".xls, .xlsx" onChange={ event => this.handleChange(event) }/>
+          </label>
+          <input type="submit" value={texts.controlsBtnValue}></input>
         </form>
       </div>
     );
---
+++ src/constants.js
@@ -0,0 +1,20 @@
+const styles = {
+  container: {
+    textAlign: 'left',
+    margin: '20px'
+  },
+  input: {
+    marginBottom: '20px',
+  }
+}
+
+const texts = {
+  reportsLabel: 'Browse report files:',
+  outputLabel: 'Browse output file:',
+  controlsBtnValue: 'Run controls',
+}
+
+module.exports = {
+  styles,
+  texts
+}
